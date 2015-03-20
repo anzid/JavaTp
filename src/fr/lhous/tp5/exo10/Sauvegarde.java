0c90dc25063f29d26bc33e2f0e9fa1254d1f7a25
LRACHDI <rachdi.lhoussaine@gmail.com>
2015-03-20 19:37:27 +0100

    method sauveFichierTexte in progress

--- src/fr/lhous/tp5/exo10/Sauvegarde.java
+++ src/fr/lhous/tp5/exo10/Sauvegarde.java
@@ -1,5 +1,8 @@
 package fr.lhous.tp5.exo10;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 import fr.lhous.tp3.exo3.Marin;
 
@@ -9,11 +12,15 @@ public class Sauvegarde {
 	};
 	
 	public static void sauveFichierTexte(String nomFichier, Marin marin){
-		File f = new File(nomFichier + ".txt");
+		try {
+			FileOutputStream fos = new FileOutputStream(new File(nomFichier + ".txt"));
+			DataOutputStream dos = new DataOutputStream(fos);
+			String buffer = marin.getNom()+"|"+marin.getPrenom()+"|"+marin.getSalaire();
+			dos.writeByte();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		
 		
 	}
-	
-	
-
 }

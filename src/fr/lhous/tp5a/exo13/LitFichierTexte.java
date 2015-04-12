3bc2258b837ba1c6021c624f9ecc50b82324b305
LRACHDI <rachdi.lhoussaine@gmail.com>
2015-04-12 23:57:43 +0200

    updated exo13 tp5a

--- src/fr/lhous/tp5a/exo13/LitFichierTexte.java
+++ src/fr/lhous/tp5a/exo13/LitFichierTexte.java
@@ -4,9 +4,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
-import java.util.SortedSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
+
 
 public class LitFichierTexte {
 	
@@ -15,33 +16,49 @@ public class LitFichierTexte {
 	}
 	
 	
-	public SortedSet<String> LisFichierTexte() {
-		SortedSet<String> names = new TreeSet<String>();
+	public static List<String> LisFichierTexte() {
 		FileReader fr = null;
+		int numberLine = 0;
+		int nombreTerer = 0;
+		int nombreIlYa = 0;
+		int nombreMots = 0;
+		
+		
+		List <String> texte = new ArrayList<>();
 		try{
-			fr = new FileReader("files/names.txt");
+			fr = new FileReader("files/germinal.txt");
 			LineNumberReader lnr = new LineNumberReader(fr);
 			String line = null;
+			StringTokenizer st = new StringTokenizer(line.toString());//definir la separation entre les mots
 			do{
 				line = lnr.readLine();
 				if(line != null){
-					System.out.println(line);
-					StringTokenizer st = new StringTokenizer(line,"\",\"");
-					while(st.hasMoreTokens()){
-						names.add(st.nextToken());
+					texte.add(line);
+					numberLine ++; //nombre de ligne est : 19615
+					if(line.startsWith("-")){
+						nombreTerer ++;
+					}
+					
+					while(st.hasMoreTokens()){//tant que y'a d'autre mots a lire
+						nombreMots ++;
+					}
+					
+					if (line.contains("il y a ") || line.contains(" il y a ")){
+						nombreIlYa++;
 					}
 				}
 			}while(line != null);
-			System.out.println(names);
-
+			System.out.println("le nombre de lignes est : " + numberLine);//resultat : 19615
+			System.out.println("le nombre de terer est : " + nombreTerer);//resultat : 1356
+			System.out.println("le nombre de il y a est : " + nombreIlYa);//resultat : 18
 
 		}	catch (FileNotFoundException e) { 
 
-			// gestion de l'erreur
+			System.out.println("Erreur " + e.getMessage()) ;
 
 		}  catch (IOException e) {
 
-			// gestion de l'erreur
+			System.out.println("Erreur " + e.getMessage()) ;
 
 		}  finally {
 
@@ -54,12 +71,23 @@ public class LitFichierTexte {
 
 				}  catch (IOException e) {
 
-					// gestion de l'erreur
+					System.out.println("Erreur " + e.getMessage()) ;
 				}
 			}
 		}
-		return names;
-
+		return texte;
+	}
+	
+	public static int nombreMotsDansFichier(List<String> lines){
+		StringTokenizer st = new StringTokenizer(lines.toString());//definir la separation entre les mots
+		int nombreMots = 0;
+		for(String s : lines){
+			while(st.hasMoreTokens()){//tant que y'a d'autre mots a lire
+				nombreMots ++;
+			}
+		}
+		return nombreMots;
 	}
 
+	
 }
---
+++ src/fr/lhous/tp5a/exo13/TestLitFichierTexte.java
@@ -0,0 +1,12 @@
+package fr.lhous.tp5a.exo13;
+
+public class TestLitFichierTexte {
+
+	public static void main(String[] args) {
+		// les methodes sont statique 
+		System.out.println(LitFichierTexte.nombreMotsDansFichier(LitFichierTexte.LisFichierTexte()));
+		
+
+	}
+
+}
